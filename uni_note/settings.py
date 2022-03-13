9d931c78a90d27cbe19ec06e87478dfba5533c58
michal <samordak85@gmail.com>
2022-03-13 14:21:23 +0000

    corrected urls

--- uni_note/settings.py
+++ uni_note/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%it&#j+fu11y6wt8v*!jy=to3-h$op@(f#wxjp5&p+en+8v-86'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-# ALLOWED_HOSTS = ['127.0.0.1','uninotes.vhome.local', 'localhost']
+#ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
