d73f0771914fe632c49102a103dc44531ee9e8bf
michal <samordak85@gmail.com>
2022-03-13 12:38:52 +0000

    poprawione url

--- uni_note/urls.py
+++ uni_note/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls', namespace='users')),
-    path('uni_notes/', include('uni_notes.urls', namespace='uni_notes')),
+    path('', include('uni_notes.urls', namespace='uni_notes')),
     path('contact/', include('mailer.urls', namespace='contact'))
 ]
